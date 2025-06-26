b4eacace0de85fa31367cbcc1df7f0582299ccfc
Tristan-P3 <tpeetre@gmail.com>
2025-06-26 20:20:37 +0200

    Personalized

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 export default class Footer extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <footer>
-      <div className="row">
-        <div className="twelve columns">
-          <ul className="social-links">
-            {
-              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
-                return(
-                  <li>
-                    <a href={item.url}>
-                    <i className={item.className} />
-                    </a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-          
-        </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
-      </div>
-    </footer>
-    );
-  }
+	render() {
+		let resumeData = this.props.resumeData;
+		return (
+			<footer>
+				<div className="row">
+					<div className="twelve columns">
+						<ul className="social-links">
+							{
+								resumeData.socialLinks && resumeData.socialLinks.map((item) => {
+									return (
+										<li>
+											<a href={item.url}>
+												<i className={item.className} />
+											</a>
+										</li>
+									)
+								})
+							}
+						</ul>
+
+					</div>
+					<div className="footer-contact" style={{ textAlign: 'center', marginTop: '10px' }}>
+						<a href="mailto:tristan.peetre@gmail.com" style={{ color: '#fff', textDecoration: 'none' }}>
+							<i className="fa fa-envelope" style={{ marginRight: '6px' }}></i>tristan.peetre@gmail.com
+						</a>
+					</div>
+					<div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+				</div>
+			</footer>
+		);
+	}
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,68 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+	"imagebaseurl": "https://tristan-p3.github.io/",
+	"name": "Tristan Peetre",
+	"role": "Fullstack Developer",
+	"linkedinId": "tristan-p",
+	"email": "tpeetre@gmail.com",
+	"roleDescription": "I enjoy building both user interfaces and backend systems, exploring the overlap between design and engineering. I like thinking about how systems work, scale, and stay maintainable, and I'm especially interested in interactive design, game mechanics, and developer tooling.",
+	"socialLinks": [
+		{
+			"name": "linkedin",
+			"url": "https://www.linkedin.com/in/tristan-p/",
+			"className": "fa fa-linkedin"
+		},
+		{
+			"name": "github",
+			"url": "https://github.com/Tristan-P3",
+			"className": "fa fa-github"
+		}
+	],
+	"aboutme": "I recently completed my Master of Science in Engineering at Lund University (LTH), specializing in Information and Communication Engineering Technologies with a focus on software. My studies covered software engineering, programming, web and interaction design, security, databases, and systems design. My master's thesis focused on researching local deployment of an LLM chatbot. I'm passionate about both the user-facing and technical sides of development, and I enjoy working on projects that combine usability with solid engineering. My interests include fullstack development, backend design, developer tooling, and game systems. In my free time, I enjoy film/TV, board games, video games, and video editing.",
+	"address": "Lund, Sweden",
+	"website": "https://tristan-p3.github.io/",
+	"education": [
+		{
+			"UniversityName": "Lund University - LTH",
+			"programme": "Master of Science in Engineering, Information and Communication Engineering Technologies",
+			"specialization": "Software (focus on software, programming, web/interaction design, security, databases, and systems design)",
+			"MonthOfPassing": "Jun",
+			"YearOfPassing": "2025",
+			"Achievements": "Completed a master's thesis and internship focused on software engineering and systems design, with thesis research on local deployment of an LLM chatbot."
+		}
+	],
+	"work": [],
+	"skillsDescription": "I have experience with a wide range of languages and frameworks, and enjoy working across the stack—from user interfaces and UX to backend logic, APIs, and databases. I prefer working in VS Code, but have also used Visual Studio, Eclipse, and IntelliJ.",
+	"skills": [
+		{ "skillname": "Java" },
+		{ "skillname": "C++" },
+		{ "skillname": "Python" },
+		{ "skillname": "JavaScript" },
+		{ "skillname": "TypeScript" },
+		{ "skillname": "React" },
+		{ "skillname": "Angular" },
+		{ "skillname": "HTML" },
+		{ "skillname": "CSS" },
+		{ "skillname": "SQL" },
+		{ "skillname": "Git" }
+	],
+	"portfolio": [
+		{
+			"name": "Helldodger",
+			"description": "A game I developed for a course project in Unity, a 2D survival single-player game where you avoid waves of enemies only using one button. It's available free on itch.io.",
+			"imgurl": "images/portfolio/helldodger.gif"
+		},
+		{
+			"name": "Tree Surgeon",
+			"description": "A game I developed for a course project in Unity, a 3D roguelike where you chop down trees to make yourself better at chopping down trees. It's available free on itch.io.",
+			"imgurl": "images/portfolio/tree_surgeon.gif"
+		}
+	],
+	"testimonials": [
+		{
+			"description": "This is a sample testimonial",
+			"name": "Some technical guy"
+		},
+	]
+}
+
+export default resumeData
